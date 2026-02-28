aad181c077aff350de5a2c6bec4a4ef06e64be58
Jérôme Gurhem <jerome.gurhem@hotmail.fr>
2026-02-28 14:08:03 +0100

    fix: connect

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# docker exec -it monia psql -U monia -d monia
-docker exec -it monia pg_dump -U monia -d monia
+docker exec -it monia psql -U monia -d monia
+# docker exec -it monia pg_dump -U monia -d monia
